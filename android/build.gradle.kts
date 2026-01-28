4f38b2a4a840b7593dbc39bf7344051f0a180391
Fabricio Rassier Vargas <fabriciorassier@gmail.com>
2026-01-27 21:57:02 -0300

    Commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("com.android.library") version "8.1.0" apply false
+    // MUDE AQUI DE 8.1.0 PARA 8.11.1
+    id("com.android.application") version "8.11.1" apply false
+    
+    // SE TIVER ESSE ABAIXO, MUDE TAMBÉM
+    id("com.android.library") version "8.11.1" apply false
+    
+    // O Kotlin pode deixar como está (1.9.0 ou similar)
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
